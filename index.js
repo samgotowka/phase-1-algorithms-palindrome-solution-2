aaa6deba22ae60c8d08354a7e78e3fc1b785fdd5
Sam Gotowka <sam.gotowka@gmail.com>
2022-08-23 17:10:03 -0400

    commit tree 2a372a1b942fd9e88c0dd1878ad144a1eaf23e64

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i <word.length / 2; i++ ) {
+    const j = word.length - 1- i
+    const startChar = word[i]
+    const endChar = word[j]
+    if( startChar !== endChar) return false
+  } 
+  return true
 }
 
 /* 
   Add your pseudocode here
-*/
+
+  iterate from the beginning to the middle
+  check each letter to the corresponding letter from the end
+  if any of the letters don't match return false
+      return true
 
 /*
   Add written explanation of your solution here
